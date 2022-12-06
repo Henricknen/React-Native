82d3a6e12604caa2ae42d6dd330c531d26b5ad18
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-12-05 22:36:03 -0300

    implementando_componenete_de_funcao_e_componente_de_classe_em_codigo_basico_de_'react_native'

---
+++ componentes/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import {Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import useColorScheme from 'react-native/Libraries/Utilities/useColorScheme';
+
+export default class extends Component {
+  render() {    // na 'classe' tudo é adiçionado dentro do 'render'
+    return(
+      <View>
+        <Text>React Native</Text>
+      </View>
+    );
+  }
+}
+
+// export default function App() {   // componente de função
+//   return (
+//   <View>    
+//     <Text>Luis</Text>
+//     <Text>Henrique</Text>
+//     <Text>Siva</Text>
+//     <Text>Ferreira</Text>
+//   </View>
+  // );
+// };
