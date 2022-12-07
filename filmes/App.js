d89b52858b5fe36faa2a55f282ee08f582d6aadd
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-12-06 22:44:26 -0300

    iniciando_projeto_'mobile'_de_fimes_em_cartaz_ultilizando_'react-native'

---
+++ filmes/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function App() {    // componente 'App'
+  return(
+    <View style = {{marginTop: 100}}>
+      <Text style = {{color: 'red', fontSize:30}}>Luis henrique</Text>
+      <Text>Full Stack</Text>
+    </View>
+  )
+}
+
+export default App;   // wxpotando componente 'App'
